c48b0bd9d588eff902787e4f4ee5375b38f2fc20
ExponGTR <51163458+ExponGTR@users.noreply.github.com>
2026-04-25 15:55:29 +0530

    Minor variable name changes

--- processor/instructionMem.v
+++ processor/instructionMem.v
@@ -2,9 +2,9 @@ module instructionMem (
     input [31:0] pc,
     output [31:0] instr
 );
-    reg [31:0] memory [0:2047];
+    reg [31:0] mem [0:2047];
     initial begin
-        $readmemh("prog.hex", memory);
+        $readmemh("C:\Users\expon\OneDrive\Desktop\Emulation\C-mpleRisc\processor\prog.hex", mem);
     end
 
     //since instr size is 4 bytes, last 2 bits are for byte offset
